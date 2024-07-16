397f33bcdb709af66a85844d018904a4ac6806e7
dim5ptr <dimasetya1836@gmail.com>
2024-07-16 14:53:37 +0700

    Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,6 +38,11 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+        $request->validate([
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:6'],
+        ]);
+
         $response = Http::post(env('POSTMAN_BASE_URL') . '/login', [
             'email' => $request->email,
             'password' => $request->password,
